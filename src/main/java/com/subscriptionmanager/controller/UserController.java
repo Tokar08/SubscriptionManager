d8bde5375fe60f8651d5f9e47f4b031810990799
Tokar08 <ivan.tokar.git@gmail.com>
2024-06-04 23:46:34 +0300

    Version before adding Spring Security

--- src/main/java/com/subscriptionmanager/controller/UserController.java
+++ src/main/java/com/subscriptionmanager/controller/UserController.java
@@ -6,7 +6,6 @@ import com.subscriptionmanager.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
